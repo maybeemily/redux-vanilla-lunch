4492c3b0b2905f67dd83f8c2ce9b27bb4578d25f
emily baier <emilyrbaier@gmail.com>
2019-05-22 15:49:35 -0700

    bonus - chips drinks and sandwiches are arrays

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 import { createStore } from 'redux';
 
+//use array methods that return new arrays 
+
 const initialState = {
-  drink: null,
-  sandwich: null,
-  chips: null
+  drink: [],
+  sandwich: [],
+  chips: []
 };
 
 function reducer(state = initialState, action) {
   switch(action.type) {
     case 'ADD_DRINK':
-      return { ...state, drink: action.payload };
+      return { ...state, drink: [...state.drink, action.payload] };
     case 'ADD_SANDWICH':
-      return { ...state, sandwich: action.payload };
+      return { ...state, sandwich: [...state.sandwich, action.payload] };
     case 'ADD_CHIPS':
-      return { ...state, chips: action.payload };
+      return { ...state, chips: [...state.chips, action.payload] };
     case 'REMOVE_DRINK':
-      return { ... state, drink: null };
+      //return { ... state, drink: [...state.drink, action.payload] };
+      return  { ...state, drink: state.drink.filter(drink => { 
+        return drink != action.payload; }) }; //if drink doesn't equal the payload keep it})
     case 'REMOVE_SANDWICH':
-      return { ...state, sandwich: null };
+      return { ...state, sandwich: state.sandwich.filter(sandwich => { 
+        return sandwich != action.payload; }) };
     case 'REMOVE_CHIPS': 
-      return { ...state, chips: null };
+      return { ...state, chips: state.chips.filter(chips => { 
+        return chips != action.payload; }) };
     case 'EMPTY_LUNCHBOX':
-      return { drink: null, sandwich: null, chips: null };
+      return { drink: [], sandwich: [], chips: [] };
     default:
       return state;
   }
@@ -35,6 +41,11 @@ store.dispatch({
   payload: 'cola'
 });
 
+store.dispatch({
+  type: 'ADD_DRINK',
+  payload: 'root beer'
+});
+
 console.log('drink added', store.getState());
 
 store.dispatch({
@@ -42,7 +53,22 @@ store.dispatch({
   payload: 'bologna'
 });
 
+store.dispatch({
+  type: 'ADD_SANDWICH',
+  payload: 'avocado'
+});
+
+store.dispatch({
+  type: 'ADD_SANDWICH',
+  payload: 'pb&j'
+});
+
 console.log('sandwich added', store.getState());
+
+store.dispatch({
+  type: 'ADD_CHIPS',
+  payload: 'sun chips'
+});
 
 store.dispatch({
   type: 'ADD_CHIPS',
@@ -52,19 +78,22 @@ store.dispatch({
 console.log('chips added', store.getState());
 
 store.dispatch({
-  type: 'REMOVE_CHIPS'
+  type: 'REMOVE_CHIPS',
+  payload: 'fritos'
 });
 
 console.log('chips removed', store.getState());
 
 store.dispatch({
-  type: 'REMOVE_SANDWICH'
+  type: 'REMOVE_SANDWICH',
+  payload: 'bologna'
 });
 
 console.log('sandwich removed', store.getState());
 
 store.dispatch({
-  type: 'REMOVE_DRINK'
+  type: 'REMOVE_DRINK',
+  payload: 'cola'
 });
 
 console.log('drink removed', store.getState());
