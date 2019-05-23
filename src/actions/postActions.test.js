e2086f45a9105e1dcfd1b57ff6ef48b02f655ae4
emily baier <emilyrbaier@gmail.com>
2019-05-23 10:05:16 -0700

    it passes, but doesnt make sense yet because i would never remove a post based on the title and body, i want to use an id or array index

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -2,15 +2,15 @@ import { addPost, ADD_POST, removePost, REMOVE_POST } from './postActions';
 
 describe('post actions', () => {
   it('creates a post with a title and body', () => {
-    expect(addPost({ title: 'title', body: 'body', id: 1 })).toEqual({
+    expect(addPost({ title: 'title', body: 'body' })).toEqual({
       type: ADD_POST,
-      payload: { title: 'title', body: 'body', id: 1 }
+      payload: { title: 'title', body: 'body' }
     });
   });
   it('removes a post', () => {
-    expect(removePost({ title: 'title', body: 'body', id: 1 })).toEqual({
+    expect(removePost({ title: 'title', body: 'body' })).toEqual({
       type: REMOVE_POST,
-      payload: { title: 'title', body: 'body', id: 1 }
+      payload: { title: 'title', body: 'body' }
     });
   });
 });
