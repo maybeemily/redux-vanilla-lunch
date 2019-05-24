9a64419515789dfcef7e23698879865dbd21054e
emily baier <emilyrbaier@gmail.com>
2019-05-24 16:54:57 -0700

    refactored test

--- src/selectors/postSelectors.test.js
+++ src/selectors/postSelectors.test.js
@@ -6,32 +6,38 @@ describe('post selectors', () => {
       posts: {
         1234: {
           title: 'cool title',
-          body: 'cool body'
+          body: 'cool body',
+          id: 1234
         },
         1235: {
           title: 'cool title2',
-          body: 'cool body'
+          body: 'cool body',
+          id: 1235
         },
         1236: {
           title: 'cool title3',
-          body: 'cool body'
+          body: 'cool body',
+          id: 1236
         }
       }
     };
-    expect(getPosts(state)).toEqual({
-      1234: {
+    expect(getPosts(state)).toEqual([
+      {
         title: 'cool title',
-        body: 'cool body'
+        body: 'cool body',
+        id: 1234
       },
-      1235: {
+      {
         title: 'cool title2',
-        body: 'cool body'
+        body: 'cool body',
+        id: 1235
       },
-      1236: {
+      {
         title: 'cool title3',
-        body: 'cool body'
-      } 
-    });
+        body: 'cool body',
+        id: 1236
+      }
+    ]);
   });
 
   it('gets post by id', () => {
