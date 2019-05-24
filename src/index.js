a0070a0b61b0c82e668a8f1fd713ccd3a16412cc
emily baier <emilyrbaier@gmail.com>
2019-05-24 15:26:37 -0700

    set up store, app renders

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-import { createStore } from 'redux';
-import { addPost } from './actions/postActions';
-import reducer from './reducers';
-//import { addComment } from './actions/commentActions';
-//cannot mutate the original state. always need to hand it a new state
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+render(
+  <App/>,
+  document.getElementById('root')
 );
-
-store.dispatch(addPost('cool blog post', 'this is a body'));
-store.dispatch(addPost('another blog post', 'my body my body'));
-console.log('post added', store.getState());
-
-// const id = Object.keys(store.getState().posts)[0];
-// console.log(Object.values(id));
-// store.dispatch(addComment(id, 'my comment'));
-
-console.log('comment added', store.getState());
-// store.dispatch(removePost(id));
-// console.log('post removed', store.getState());
